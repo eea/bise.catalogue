8daa707c464aa7ecc9a14939d12bf24519119b3d
Jon Arrien <jon@xdsl-89-0-13-15.netcologne.de>
2012-12-18 09:11:05 +0100

    Added database seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+contents = [
+'Lorem ipsum dolor sit amet.',
+'Consectetur adipisicing elit, sed do eiusmod tempor incididunt.',
+'Labore et dolore magna aliqua.',
+'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+'Excepteur sint occaecat cupidatat non proident.'
+]
+
+puts "Deleting all articles..."
+Article.delete_all
+
+unless ENV['COUNT']
+
+  puts "Creating articles..."
+  %w[ One Two Three Four Five ].each_with_index do |title, i|
+    Article.create :title => title, :content => contents[i], :published_on => i.days.ago.utc
+  end
+
+else
+
+  puts "Creating 10,000 articles..."
+  (1..ENV['COUNT'].to_i).each_with_index do |title, i|
+    Article.create :title => "Title #{title}", :content => 'Lorem', :published_on => i.days.ago.utc
+    print '.'
+  end
+
+end
