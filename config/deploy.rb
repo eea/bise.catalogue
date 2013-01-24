a482dd21a0fc48b9a0235fd648cfaf4721c1d97e
Jon Arrien <jonarrien@gmail.com>
2013-01-24 11:42:24 +0100

    Fix deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,14 @@ require "bundler/capistrano"
 # server "192.168.1.215", :web, :app, :db, primary: true
 
 set :application, "catalogue"
-set :user, "deployer"
+
 set :repository,  "git@i3apps.sytes.net:jonarrien/catalogue.git"
 set :branch, "master"
+
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
