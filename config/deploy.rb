7e4af09099db8149b0f88628736fb6a072855ae7
Jon Arrien <jonarrien@gmail.com>
2013-01-24 15:41:57 +0100

    Added tinymce stylesheets

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,14 @@ after "deploy:restart", "deploy:cleanup"
 #   end
 # end
 
+before "deploy:create_symlink", "assets:precompile"
+
+namespace :assets do
+    desc "Compile assets"
+    task :precompile, :roles => :app do
+        run "cd #{release_path} && rake RAILS_ENV=#{rails_env} assets:precompile"
+    end
+end
 
 namespace :deploy do
 
