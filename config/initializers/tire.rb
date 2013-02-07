c70b18695fa9a76be8f66a4a0b4bc5915ae0e72a
Jon Arrien <jonarrien@gmail.com>
2013-02-07 13:20:14 +0100

    Create a prefix for ElasticSearch based on app and environment

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -5,7 +5,8 @@ Tire.configure do
     # Tire::Model::Search.index_prefix(prefix)
 end
 
-if Rails.env.test?
-    prefix = "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}_"
-    Tire::Model::Search.index_prefix(prefix)
-end
+# app_name = Rails.application.class.parent_name.underscore.dasherize
+# app_env = Rails.env
+# INDEX_NAME = "#{app_name}-#{app_env}"
+
+Tire::Model::Search.index_prefix "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}"
