29e1e7dc48ca5517c787e56c6945eca3bf374540
Jon Arrien <jonarrien@gmail.com>
2013-02-13 13:04:24 +0100

    Removed tinymce_assets function 'cuz is global

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,9 +47,6 @@ Catalogue::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( articles.js documents.js documents.css)
-  config.assets.precompile += %w( tinymce/advanced/skings/thebigreason/ui.css )
-  config.assets.precompile += %w( tinymce/advanced/skings/thebigreason/dialog.css )
-  config.assets.precompile += %w( tinymce/advanced/skings/thebigreason/content.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
