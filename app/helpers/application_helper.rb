8e56f2e6edd5de5adc3499c8739c5def19770ba0
Jon Arrien <jonarrien@gmail.com>
2013-03-06 12:07:11 +0100

    Added Countries and Biographical regions to Application Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,52 @@
 module ApplicationHelper
+
+    def countries
+        [
+            'Austria',
+            'Belgium',
+            'Bulgaria',
+            'Cyprus',
+            'Czech Republic',
+            'Germany',
+            'Denmark',
+            'Estonia',
+            'Spain',
+            'Finland',
+            'France',
+            'Greece',
+            'Hungary',
+            'Ireland',
+            'Italy',
+            'Lithuania',
+            'Luxembourg',
+            'Latvia',
+            'Malta',
+            'Netherlands',
+            'Poland',
+            'Portugal',
+            'Romania',
+            'Sweden',
+            'Slovenia',
+            'Slovakia',
+            'United Kingdom'
+        ]
+    end
+
+    def biographical_regions
+        [
+            'Alpine',
+            'Anatolian',
+            'Artic',
+            'Atlantic',
+            'Black sea',
+            'Boreal',
+            'Continental',
+            'Macaronesia',
+            'Mediterranean',
+            'Pannonian',
+            'Steppic',
+            'Region not detailed'
+        ]
+    end
+
 end
