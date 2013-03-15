e73ac0eeb4a921699679d35bff3e660e7638f028
Jon Arrien <jonarrien@gmail.com>
2013-03-15 10:38:28 +0100

    Documents controller spec changed

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -24,14 +24,18 @@ describe DocumentsController do
   # Document. As you add validations to Document, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      :name => 'Example Document',
+      :author => 'jon',
+      :file => File.open('/Users/jon/Practical_Vim.pdf')
+    }
   end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # DocumentsController. Be sure to keep this updated too.
   def valid_session
-    {}
+    {"warden.user.user.key" => session["warden.user.user.key"]}
   end
 
   describe "GET index" do
