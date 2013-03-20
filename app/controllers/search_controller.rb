e3e635e7e2d45bcd5b0cb321b873bd1731d98c95
Jon Arrien <jonarrien@gmail.com>
2013-03-20 10:37:07 +0100

    Fix for cells in search#index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,31 @@ class SearchController < ApplicationController
         puts ":: search#index"
         q = params[:query]
         q = nil if q == ''
-        # binding.pry
+
+        indexes = nil
+        if Rails.env.production?
+            indexes = %w(catalogue_production_articles catalogue_production_documents catalogue_production_species)
+        elsif Rails.env.development?
+            indexes = %w(catalogue_development_articles catalogue_development_documents catalogue_development_species)
+        end
 
         if !q.nil?
-            @rows = Tire.search %w(catalogue_production_articles catalogue_production_documents catalogue_production_species), :laod => true, :page => params[:page], :per_page => 30 do
+            @rows = Tire.search indexes, :laod => true, :page => params[:page], :per_page => 30 do
                 query do
                     boolean do
-                        should   { string 'title:' + q } # if not q.nil?
+                        # Article & Documents titles
+                        should   { string 'title:' + q }
+
+                        # Species scientifi name
+                        should   { string 'scientific_name:' + q }
+
                         # should   { string 'content:' + params[:query].to_s }
                         # must_not { string 'published:0' }
                     end
                 end
             end
         else
-            @rows = Tire::Results::Collection.new
+            @rows = nil
         end
 
 
