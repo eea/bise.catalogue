2d523494fb938eda782f3e9958fab0b72a961e5c
arriejon <arriejon@c3cbec39-50e6-0310-9736-c17646cd0be1>
2013-04-24 15:17:01 +0000

    Ecosystem Assesment
    git-svn-id: https://svn.eionet.europa.eu/repositories/Biodiversity/BISE_components/Catalogue/trunk@1067 c3cbec39-50e6-0310-9736-c17646cd0be1

---
+++ app/controllers/api/v1/ecosystem_assessments_controller.rb
@@ -0,0 +1,40 @@
+module Api
+    module V1
+        class EcosystemAssessmentsController < ApplicationController
+            respond_to :json
+
+            def index
+                rows = EcosystemAssessment.find(:all, :order => "id desc", :limit => 3)
+                res = {
+                    :count => EcosystemAssessment.all.size,
+                    :last_3_assessment => rows
+                }
+                respond_with res
+              # respond_with EcosystemAssessment.all.size
+            end
+
+            def show
+              respond_with EcosystemAssessment.find(params[:id])
+            end
+
+            def create
+                errors = Array.new
+                # if params.size > 0
+                #     respond_with EcosystemAssessment.new
+                # else
+                #     respond_with errors.to_json
+                # end
+                respond_with EcosystemAssessment.create(params[:ecosystem_assessment])
+            end
+
+            def update
+              respond_with EcosystemAssessment.update(params[:id], params[:ecosystem_assessments])
+            end
+
+            def destroy
+              respond_with EcosystemAssessment.destroy(params[:id])
+            end
+        end
+    end
+end
+
