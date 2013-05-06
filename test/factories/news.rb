35ad320706d5357ea9db7b6f4f5f305cd759e6ef
garcinur <garcinur@c3cbec39-50e6-0310-9736-c17646cd0be1>
2013-05-06 08:54:34 +0000

    Links+News
    git-svn-id: https://svn.eionet.europa.eu/repositories/Biodiversity/BISE_components/Catalogue/trunk@1102 c3cbec39-50e6-0310-9736-c17646cd0be1

---
+++ test/factories/news.rb
@@ -0,0 +1,18 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :news do
+    approved "MyString"
+    approved_at "MyString"
+    author "MyString"
+    english_title "MyString"
+    language "MyString"
+    published_on "MyString"
+    source "MyString"
+    title "MyString"
+    url "MyString"
+    description "MyString"
+    comment "MyString"
+    published "MyString"
+  end
+end
