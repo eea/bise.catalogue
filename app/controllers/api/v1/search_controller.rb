3466a953a5bd4666eb7468f23376c38dbae2ea84
arriejon <arriejon@c3cbec39-50e6-0310-9736-c17646cd0be1>
2013-05-28 14:27:58 +0000

    Added some filter and facet_filters to global search
    git-svn-id: https://svn.eionet.europa.eu/repositories/Biodiversity/BISE_components/Catalogue/trunk@1191 c3cbec39-50e6-0310-9736-c17646cd0be1

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -46,6 +46,18 @@ module Api
                     per  = if params[:per_page].present? then params[:per_page].to_i else 10 end
                     from = if page == 1 then 0 else (page - 1) * per end
 
+                    site   = params[:sites] if params[:sites].present?
+                    author = params[:authors] if params[:authors].present?
+                    # binding.pry
+
+                    search_filter = []
+                    search_filter << { :term => { 'site.name' => params[:sites] }} if params[:sites].present?
+                    search_filter << { :term => { :author => params[:authors] }} if params[:authors].present?
+                    # search_filter << { :term => { 'countries.name' => params[:countries].split(/\//) }} if params[:countries].present?
+                    # search_filter << { :term => { 'languages.name' => params[:languages].split(/\//) }} if params[:languages].present?
+                    # search_filter << { :term => { :biographical_region => params[:biographical_region] }} if params[:biographical_region].present?
+                    # search_filter << { :range=> { :published_on => { :gte => date_init , :lt => date_end }}} if params[:published_on].present?
+
                     @rows = Tire.search indexes, :load => false, :from => from, :size => per do
                         query do
                             boolean do
@@ -55,29 +67,38 @@ module Api
                                 # Species scientifi name
                                 should   { string 'scientific_name:' + q }
 
+                                # must     { string 'author:' + author } unless author.nil?
                                 # should   { string 'content:' + params[:query].to_s }
                                 # must_not { string 'published:0' }
                             end
                         end
 
+                        filter :term, 'site.name' => site unless site.nil?
+                        filter :term, :author => author unless author.nil?
+
                         facet 'sites' do
                             terms 'site.name'
+                            facet_filter :and, search_filter unless search_filter.empty?
                         end
 
                         facet 'authors' do
                             terms :author
+                            facet_filter :and, search_filter unless search_filter.empty?
                         end
 
                         facet 'countries' do
                             terms 'countries.name'
+                            facet_filter :and, search_filter unless search_filter.empty?
                         end
 
                         facet 'biographical_regions' do
                             terms :biographical_region
+                            facet_filter :and, search_filter unless search_filter.empty?
                         end
 
                         facet 'languages' do
                             terms 'languages.name'
+                            facet_filter :and, search_filter unless search_filter.empty?
                         end
 
                         # facet('timeline') do
