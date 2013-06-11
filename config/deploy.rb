c5d349d20e770a15d32b5a309b47ad332becbc48
Jon Arrien <jonarrien@gmail.com>
2013-06-11 10:34:09 +0200

    added line to create releases folder in deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,93 @@
+require 'capistrano-rbenv'
+require "bundler/capistrano"
+
+set :application, "catalogue"
+
+# set :repository,  "git@i3apps.sytes.net:bilbomatica/catalogue.git"
+set :repository,  "https://github.com/eea/bise.catalogue.git"
+set :branch, "master"
+
+set :user, "deployer"
+set :password, "saretex1"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :shared_children, shared_children + %w{public/uploads}
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "89.0.1.129"                          # Your HTTP server, Apache/etc
+role :app, "89.0.1.129"                          # This may be the same as your `Web` server
+role :db,  "89.0.1.129", :primary => true        # This is where Rails migrations will run
+role :db,  "89.0.1.129"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+before "deploy:create_symlink", "assets:precompile"
+
+namespace :assets do
+    desc "Compile assets"
+    task :precompile, :roles => :app do
+        run "cd #{release_path} && rake RAILS_ENV=#{rails_env} assets:precompile"
+    end
+end
+
+namespace :deploy do
+
+    before "deploy:cold", "deploy:install_bundler"
+
+    task :install_bundler, :roles => :app do
+        run "type -P bundle &>/dev/null || { gem install bundler --no-rdoc --no-ri; }"
+        run "mkdir -p ~/apps/#{application}/releases"
+    end
+
+    %w[start stop restart].each do |command|
+        desc "#{command} unicorn server"
+        task command, :roles => :app, :except => {:no_release => true} do
+            run "/etc/init.d/unicorn_#{application} #{command}"
+        end
+    end
+
+    task :setup_config, :roles => :app do
+        sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+        sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+        run "mkdir -p #{shared_path}/config"
+        put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+        puts "Now edit the config files in #{shared_path}."
+    end
+    after "deploy:setup", "deploy:setup_config"
+
+    task :symlink_config, :roles => :app do
+        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+    after "deploy:finalize_update", "deploy:symlink_config"
+
+    desc "Make sure local git is in sync with remote."
+    task :check_revision, :roles => :web do
+        unless `git rev-parse HEAD` == `git rev-parse origin/master`
+            puts "WARNING: HEAD is not the same as origin/master"
+            puts "Run `git push` to sync changes."
+            exit
+        end
+    end
+    before "deploy", "deploy:check_revision"
+
+end
