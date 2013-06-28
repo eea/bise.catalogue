ad07c6494ca47c67942287fe1411d35d38f0f229
Jon Arrien <jonarrien@gmail.com>
2013-06-28 10:55:44 +0200

    Article Model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,34 +3,33 @@ require 'spec_helper'
 
 describe Article do
 
-    it "is invalid without site" do
-        should validate_presence_of(:site)
-    end
+  before do
+    @article = FactoryGirl.create(:article)
+  end
 
-    it "is invalid without title" do
-        should validate_presence_of(:title)
-    end
+  it "is invalid without site" do
+    should validate_presence_of(:site)
+  end
 
-    it "is invalid without english title" do
-        should validate_presence_of(:english_title)
-    end
+  it "is invalid without title" do
+    should validate_presence_of(:title)
+  end
 
-    it "is invalid without author" do
-        should validate_presence_of(:author)
-    end
+  it "is invalid without english title" do
+    should validate_presence_of(:english_title)
+  end
 
-    it "is invalid without assigned language" do
-        should validate_presence_of(:language_ids)
-    end
+  it "is invalid without author" do
+    should validate_presence_of(:author)
+  end
 
-    it "is a valid article" do
-        FactoryGirl.create(:article).should be_valid
-    end
+  it "is invalid without assigned language" do
+    should validate_presence_of(:language_ids)
+  end
 
-    it "is valid without site" do
-        article = FactoryGirl.create(:article)
-        article.site = nil
-        article.should be_valid
-    end
+  it "is a valid article" do
+    @article.should be_valid
+  end
+
 
 end
