0a111488c2ec5d1eb98e0eea8bdb5b8dd21e92a2
Jon Arrien <jonarrien@gmail.com>
2013-07-11 10:53:31 +0200

    Let users select first or last pagination item

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,4 +1,4 @@
 require 'will_paginate/view_helpers'
 
-WillPaginate::ViewHelpers.pagination_options[:inner_window] = 1
-WillPaginate::ViewHelpers.pagination_options[:outer_window] = 1
+WillPaginate::ViewHelpers.pagination_options[:inner_window]   = 2
+WillPaginate::ViewHelpers.pagination_options[:outer_window]   = 0
