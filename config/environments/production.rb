c0d8ba4bb572e53e0288255b09e4d54b4e73a6f4
Jon Arrien <jonarrien@gmail.com>
2013-07-17 13:21:44 +0200

    serve_statics_assets => true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,15 @@ Catalogue::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+
+
+  # config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -20,7 +25,6 @@ Catalogue::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.css_compressor = :yui
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
