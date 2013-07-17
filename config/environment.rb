d5111802cb0c737ec974f1a634ec1bf96c26aa86
Jon Arrien <jonarrien@gmail.com>
2013-07-17 09:58:58 +0200

    Encoding::UTF_8

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,9 @@ require 'yaml'
 # ES_CONFIG = Yaml.load_file("#{RAILS_ROOT}/config/elasticsearch.yml")
 ES_CONFIG = YAML::load_file("#{Rails.root}/config/elasticsearch.yml")
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the rails application
 Catalogue::Application.initialize!
 
