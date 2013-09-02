c65b4ba11fb751b1ac710e4a0dcff3a074b1ad4a
Jon Arrien <jonarrien@gmail.com>
2013-09-02 13:32:24 +0200

    Added Classifiable concern to news but still pending

--- app/assets/javascripts/news.js.coffee
+++ app/assets/javascripts/news.js.coffee
@@ -2,5 +2,8 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $ ->
-    $('#news_published_on').datepicker({ dateFormat: 'dd/mm/yy' })
-    $('#news_approved_at').datepicker({ dateFormat: 'dd/mm/yy' })
+  $('#news_published_on').datepicker({ dateFormat: 'dd/mm/yy' })
+  $('#news_approved_at').datepicker({ dateFormat: 'dd/mm/yy' })
+  $('#news_language_ids').chosen({
+    width: '100%'
+  })
--- app/models/news.rb
+++ app/models/news.rb
@@ -3,32 +3,27 @@ class News < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
 
-  attr_accessible :title
-  attr_accessible :english_title
-  attr_accessible :author
+  include Classifiable
 
-  attr_accessible :language_ids
-  has_and_belongs_to_many :languages, :class_name => "Language", :join_table => "news_languages", :foreign_key => "news_id"
-
-  attr_accessible :approved
-  attr_accessible :approved_at
+  # attr_accessible :approved
+  # attr_accessible :approved_at
   attr_accessible :comment
   attr_accessible :abstract
   # attr_accessible :language
-  attr_accessible :published
-  attr_accessible :published_on
-  attr_accessible :source
-  attr_accessible :url
-  attr_accessible :site_id
-  belongs_to      :site
-  attr_accessible :country_ids
-  has_and_belongs_to_many :countries, :class_name => "Country", :join_table => "newss_countries", :foreign_key => "news_id"
-  attr_accessible :biographical_region
+  # attr_accessible :published
+  # attr_accessible :published_on
+  # attr_accessible :source
+  # attr_accessible :url
+  # attr_accessible :site_id
+  # belongs_to      :site
+  # attr_accessible :country_ids
+  # has_and_belongs_to_many :countries, :class_name => "Country", :join_table => "newss_countries", :foreign_key => "news_id"
+  # attr_accessible :biographical_region
 
-  validates_presence_of :title, :message => "cannot be blank"
+  # validates_presence_of :title, :message => "cannot be blank"
   # validates_presence_of :url, :message => "cannot be blank"
-  validates_presence_of :author, :message => "cannot be blank"
-  validates_presence_of :site, :message => "cannot be blank"
+  # validates_presence_of :author, :message => "cannot be blank"
+  # validates_presence_of :site, :message => "cannot be blank"
   # validates_format_of :url, :with => /^(((http|https):\/\/))[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
 
   index_name "#{Tire::Model::Search.index_prefix}news"
