6266ff88bd4081a63cb7d3f8111758858d8bfe94
Jon Arrien <jonarrien@gmail.com>
2013-10-03 16:10:19 +0200

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,8 +75,8 @@ namespace :deploy do
         sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
         sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
         run "mkdir -p #{shared_path}/config"
-        sudo "cp #{release_path}/config/database.example.yml #{release_path}/config/database.yml"
-        puts "Now edit the config files in #{shared_path}."
+        # sudo "cp #{current_path}/config/database.example.yml #{shared_path}/config/database.yml"
+        # puts "Now edit the config files in #{shared_path}."
     end
 
     after "deploy:finalize_update", "deploy:symlink_config"
