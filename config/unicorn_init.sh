eda07eddfcccc23e81124285f919019b8eff7e49
Jon Arrien <jonarrien@gmail.com>
2013-11-26 16:04:36 +0100

    Fix in unicorn_init for deployment path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,8 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/catalogue/current
+# APP_ROOT=/home/deployer/apps/catalogue/current
+APP_ROOT=/var/local/apps/catalogue/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
