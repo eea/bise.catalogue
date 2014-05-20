b6c84871b3b8cd45f8dbc89c4fb17f55dda635a3
Jon Arrien <jonarrien@gmail.com>
2014-05-20 11:58:38 +0200

    Added taxonomies to admin area

---
+++ app/admin/taxonomies.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Taxonomy do
+  menu parent: 'Other'
+  config.comments = false
+
+  filter :uri
+  filter :code
+  filter :name
+  filter :level
+  filter :parent_id
+  filter :created_at
+  filter :updated_at
+
+  index do
+    selectable_column
+    id_column
+    column :uri
+    column :code
+    column :name
+    column :level
+    column :parent_id
+    actions
+  end
+end
+
