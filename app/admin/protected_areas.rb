e0a781a4a1a4a45081ea71f32c8d4792973ffd5a
Jon Arrien <jonarrien@gmail.com>
2014-05-20 08:24:46 +0200

    Added fields for protected sites in admin area

--- app/admin/protected_areas.rb
+++ app/admin/protected_areas.rb
@@ -2,7 +2,18 @@ ActiveAdmin.register ProtectedArea do
   menu priority: 4, parent: 'Content'
   config.comments = false
 
-  # filter :valid_name, as: :select
+  filter :source_db
+  filter :name
+  filter :code
+  filter :iucnat
+  filter :uri
+  filter :name
+  filter :designation_year
+  filter :nuts_code
+  filter :area
+  filter :length
+  filter :long
+  filter :lat
 
   index do
     selectable_column
