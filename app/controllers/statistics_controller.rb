3cbb8c4888e186a5cb15791293c862580f0a7308
Jon Arrien <jonarrien@gmail.com>
2014-05-22 15:37:38 +0200

    Order searches by creation date

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,6 @@
 class StatisticsController < ApplicationController
   def index
-    @searches = CatalogueSearch.all.paginate(page: params[:page], per_page: 10)
+    @searches = CatalogueSearch.all.order('created_at DESC')
+                               .paginate(page: params[:page], per_page: 10)
   end
 end
