ca3ae5c55b4c94eb87ffc669acd95730145a8fac
Jon Arrien <jonarrien@gmail.com>
2014-07-03 08:41:31 +0200

    don't allow destroy documents until the are deleted from ES

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
   def initialize(user)
     can :read, :all
     if user.role_author?
-      can [:new, :create, :edit, :update, :destroy], [Article, Document, Link]
+      can [:new, :create, :edit, :update], [Article, Document, Link]
     end
     can :approve_multiple, [Article, Document, Link] if user.role_validator?
     can :manage, :all if user.role_admin?
