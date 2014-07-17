f5f044bd51534a4c8d9e87217ebb670662c1c140
Jon Arrien <jonarrien@gmail.com>
2014-07-17 12:22:53 +0200

    added schema for paper trail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702055918) do
+ActiveRecord::Schema.define(version: 20140709105154) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -576,5 +577,16 @@ ActiveRecord::Schema.define(version: 20140702055918) do
   end
 
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
