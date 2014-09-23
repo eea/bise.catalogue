d50efaee05e36c450b3c77a57b4a0d205fad9fdf
Jon Arrien <jonarrien@gmail.com>
2014-09-23 14:15:58 +0200

    Improved aImprovements in permissions per library

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,20 @@ class Ability
   def initialize(user)
     return false if user.nil?
     can :read, :all
-    can [:new, :create], [Article, Document, Link] if user.role_author?
+    # can [:new, :create], [Article, Document, Link] if user.role_author?
+
+    can :new, [Article, Document, Link] if user.role_author?
+    can :create, [Article, Document, Link] do |obj|
+      unless obj.try(:site_id).nil?
+        user.library_roles.where(site_id: obj.site_id).try(:first).try(:allowed) || false
+      else
+        false
+      end
+    end
 
     # Only editable if created by user, user is an approver or admin
-    [Article, Document, Link].each do |klazz|
-      can [:edit, :update, :delete], klazz do |obj|
-        obj.try(:creator) == user || user.role_validator? || user.role_admin?
-      end
+    can [:edit, :update, :delete], [Article, Document, Link] do |obj|
+      obj.try(:creator) == user || user.role_validator? || user.role_admin?
     end
 
     can :approve_multiple, [Article, Document, Link] if user.role_validator?
