e9ea3e73a76cb7fdb0a4fd68ef88f554e4b34036
Jon Arrien <jonarrien@gmail.com>
2014-12-03 11:30:21 +0100

    Improvements in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,65 +1,40 @@
-require 'rubygems'
-
+# require 'rubygems'
 require 'cucumber/rails'
-require 'capybara-webkit'
+require 'dotenv'
+require 'sidekiq/testing'
+# require 'capybara-webkit'
+#
 
 
 Capybara.javascript_driver = :webkit
 Capybara.default_selector = :css
-Capybara.save_and_open_page_path = File.join(Rails.root, 'tmp/capybara')
+Capybara.save_and_open_page_path = 'tmp/capybara'
+
+
 # Capybara.default_wait_time = 10
 
 ActionController::Base.allow_rescue = false
 
 begin
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
   DatabaseCleaner.strategy = :transaction
+  # DatabaseCleaner.clean_with(:truncation)
+  %w(Article Document Link News ProtectedArea Habitat Species).each do |obj|
+    eval(obj).index.delete
+    eval(obj).create_elasticsearch_index
+  end
+  Dotenv.load(Rails.root.join('Dotenv.env'))
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-Before do
-  load "#{Rails.root}/db/seeds.rb"
-  DatabaseCleaner.strategy = :truncation
-  DatabaseCleaner.start
-end
-
-After do |scenario|
-  DatabaseCleaner.clean
-  clean_es_indexes
-end
-
-def clean_es_indexes
-  Article.index.delete
-  Document.index.delete
-  Link.index.delete
-  News.index.delete
-  ProtectedArea.index.delete
-  Habitat.index.delete
-  Species.index.delete
-
-  Article.create_elasticsearch_index
-  Document.create_elasticsearch_index
-  Link.create_elasticsearch_index
-  News.create_elasticsearch_index
-  ProtectedArea.create_elasticsearch_index
-  Habitat.create_elasticsearch_index
-  Species.create_elasticsearch_index
-end
-
-# You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
-# See the DatabaseCleaner documentation for details. Example:
-#
-#   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#     # { :except => [:widgets] } may not do what you expect here
-#     # as Cucumber::Rails::Database.javascript_strategy overrides
-#     # this setting.
-#     DatabaseCleaner.strategy = :truncation
+# def clean_es_indexes
+#   %w(Article Document Link News ProtectedArea Habitat Species).each do |obj|
+#     eval(obj).index.delete
+#     eval(obj).create_elasticsearch_index
 #   end
-#
-#   Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-#
+# end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
