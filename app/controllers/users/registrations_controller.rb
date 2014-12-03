90e5ef8df77aeb0e8d7d659638dfccad7e7ac610
Jon Arrien <jonarrien@gmail.com>
2014-12-03 11:29:45 +0100

    Fix in permissions per library

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,22 +21,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-    # self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    # prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-    # if update_resource(resource, account_update_params)
-    #   yield resource if block_given?
-    #   if is_flashing_format?
-    #     flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-    #       :update_needs_confirmation : :updated
-    #     set_flash_message :notice, flash_key
-    #   end
-    #   sign_in resource_name, resource, bypass: true
-    #   respond_with resource, location: after_update_path_for(resource)
-    # else
-    #   clean_up_passwords resource
-    #   respond_with resource
-    # end
   end
 
   protected
