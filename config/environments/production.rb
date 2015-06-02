37b58b2554cdecbb1f8d3e14ba34a9e9ed278a48
Alex Eftimie <alex@eftimie.ro>
2015-06-02 16:46:34 +0200

    [environments] Use sendmail in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Catalogue::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => '194.30.43.115:3000' }
+  config.action_mailer.default_url_options = { :host => 'catalogue.biodiversity.europa.eu' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = { :arguments => '-i' }
 
   # Enable threaded mode
   # config.threadsafe!
