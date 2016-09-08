dec673f5f00601e7d02d8cebb8e4bcdc3d251460
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-09-08 11:28:23 +0300

    Better reindexing of ES

--- reindex_elasticsearch.sh
+++ reindex_elasticsearch.sh
@@ -7,12 +7,12 @@ if [ -z $RAILS_ENV ]; then
 	exit 1
 fi
 
-rake environment tire:import CLASS='Article' FORCE=true
-rake environment tire:import CLASS='BiogeoRegion' FORCE=true
-rake environment tire:import CLASS='Country' FORCE=true
-rake environment tire:import CLASS='Document' FORCE=true
-rake environment tire:import CLASS='Habitat' FORCE=true
-rake environment tire:import CLASS='Link' FORCE=true
-rake environment tire:import CLASS='News' FORCE=true
-rake environment tire:import CLASS='ProtectedArea' FORCE=true
-rake environment tire:import CLASS='Species' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Article' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='BiogeoRegion' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Country' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Document' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Habitat' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Link' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='News' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='ProtectedArea' FORCE=true
+rake environment tire:import PARAMS='{:per_page => 10}' CLASS='Species' FORCE=true
